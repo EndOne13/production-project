6006c82a3e399fdb45feb7806d1129bd94ad9a2b
Evgenyi Aboimov <tuz1k@mail.ru>
2023-02-18 15:06:13 +0300

    add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,10 +65,10 @@ module.exports = {
     globals: {
         __IS_DEV__: true,
     },
-    overrides: [{
-        files: ['**/src/**/*.test.{ts, tsx}'],
-        rules: {
-            'i18next/no-literal-string': 'off',
+    overrides: [
+        {
+            files: ['**/src/**/*.test.{ts, tsx}'],
+            rules: { 'i18next/no-literal-string': 'off' },
         },
-    }],
+    ],
 };
